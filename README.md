47404aca9be0c507a2e738748b8163cc62a2295e
Thomas Jeanneau <thomas.jeanneau.freelance@gmail.com>
2016-10-01 13:56:26 +0200

    add typeform

---
+++ README.md
@@ -0,0 +1,2 @@
+# coworking-numa
+A script that register you into the slack of Numa
--- server/main.js
+++ server/main.js
@@ -1,5 +1,26 @@
 import { Meteor } from 'meteor/meteor';
+import request from 'request';
+import { Typeform } from 'typeform-node-api';
+import cron from 'cron';
+const timeZone = Meteor.settings.timeZone;
+const typeForm_api_key = Meteor.settings.typeForm_api_key;
+const typeForm_id = Meteor.settings.typeForm_id;
+
+var typeForm_api = new Typeform(typeForm_api_key);
+
+let sendInviteForSlack = Meteor.bindEnvironment(() => {
+    typeForm_api.getFormResponses(typeForm_id, function(data){
+        console.log(data);
+    });
+});
+
+const cronInviteSlack = new cron.CronJob({
+    cronTime: '00 * * * * *',
+    onTick: sendInviteForSlack,
+    start: false,
+    timeZone: timeZone
+});
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    cronInviteSlack.start();
 });
