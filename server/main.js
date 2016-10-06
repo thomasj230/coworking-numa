d9c2209a137a654780f5f6de15485e08c2ca1e26
Thomas Jeanneau <thomas.jeanneau.freelance@gmail.com>
2016-10-06 18:07:44 +0200

    bot working

--- server/main.js
+++ server/main.js
@@ -1,17 +1,24 @@
 import { Meteor } from 'meteor/meteor';
-import request from 'request';
-import { Typeform } from 'typeform-node-api';
+import Typeform from 'typeform-node-api';
 import cron from 'cron';
+import { HTTP } from 'meteor/http';
+
 const timeZone = Meteor.settings.timeZone;
 const typeForm_api_key = Meteor.settings.typeForm_api_key;
 const typeForm_id = Meteor.settings.typeForm_id;
+const slack_token = Meteor.settings.slack_token;
 
 var typeForm_api = new Typeform(typeForm_api_key);
 
 let sendInviteForSlack = Meteor.bindEnvironment(() => {
-    typeForm_api.getFormResponses(typeForm_id, function(data){
-        console.log(data);
-    });
+    typeForm_api.getFormResponses(typeForm_id, Meteor.bindEnvironment((data) => {
+        data.responses.forEach((response) => {
+            if(response.answers.textfield_32544220){
+                var result = HTTP.post('https://numacoworking.slack.com/api/users.admin.invite?email=' + response.answers.textfield_32544220 + '&token=' + slack_token + '&set_active=true');
+                console.log(result);
+            }
+        });
+    }));
 });
 
 const cronInviteSlack = new cron.CronJob({
